72305862ec5419e2f55eb23488fcd8418512e924
Jigneshmotwani <96459160+Jigneshmotwani@users.noreply.github.com>
2025-08-28 04:06:20 +0800

    Update 9

--- setup.py
+++
@@ -1,9 +0,0 @@
-import os
-from telegram import Bot
-
-TELEGRAM_TOKEN = "8445573627:AAFh3Q8pkl-UKK-tw4NCAQD4Esmpnb0hEZ4"
-RENDER_URL = "https://loafer-tele-bot.onrender.com"  # same as your deployed URL
-
-bot = Bot(token=TELEGRAM_TOKEN)
-bot.set_webhook(f"{RENDER_URL}/{TELEGRAM_TOKEN}")
-print("Webhook set!")
